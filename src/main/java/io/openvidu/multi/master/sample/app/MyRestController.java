90fa30729b5eadffa56635943a2e4b7fd0b7660d
pabloFuente <pablofuenteperez@gmail.com>
2021-04-22 18:37:12 +0200

    Initial commit

---
+++ src/main/java/io/openvidu/multi/master/sample/app/MyRestController.java
@@ -0,0 +1,320 @@
+package io.openvidu.multi.master.sample.app;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+	private static final Logger log = LoggerFactory.getLogger(MyRestController.class);
+
+	// URL where our OpenVidu server is listening
+	private String OPENVIDU_URL;
+	// Secret shared with our OpenVidu server
+	private String SECRET;
+	// Custom http client to consume REST API not supported by openvidu-java-client
+	private HttpClient HTTP_CLIENT;
+
+	// OpenVidu object as entrypoint of the SDK
+	private OpenVidu openVidu;
+
+	// Collection of Sessions
+	private Map<String, Session> sessions = new ConcurrentHashMap<>();
+	private Set<String> crashedSessions = new HashSet<>();
+
+	public MyRestController(@Value("${openvidu.secret}") String secret, @Value("${openvidu.url}") String openviduUrl) {
+		this.SECRET = secret;
+		this.OPENVIDU_URL = openviduUrl;
+		this.openVidu = new OpenVidu(OPENVIDU_URL, SECRET);
+		this.initInsecureHttpClient();
+		log.info("Connecting to OpenVidu Pro Multi Master cluster at {}", OPENVIDU_URL);
+	}
+
+	/**
+	 * This method creates a Connection for an existing or new Session, and returns
+	 * the Connection's token to the client side
+	 */
+	@RequestMapping(value = "/get-token", method = RequestMethod.POST)
+	public ResponseEntity<?> getToken(@RequestBody Map<String, Object> params) {
+
+		log.info("Getting sessionId and token | {sessionId}={}", params);
+
+		// The Session to connect
+		String sessionId = (String) params.get("sessionId");
+
+		if (this.crashedSessions.contains(sessionId)) {
+
+			// Session is flagged as crashed and is being rebuilt
+			log.warn("A user asked for a token, but Session {} has crashed and is being rebuilt", sessionId);
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+
+		} else if (this.sessions.containsKey(sessionId)) {
+
+			// Session already exists
+			log.info("Existing session {}", sessionId);
+			return returnToken(this.sessions.get(sessionId));
+
+		} else {
+
+			// Session does not exist yet
+			log.info("New session {}", sessionId);
+			return createSessionAndReturnToken(sessionId);
+
+		}
+	}
+
+	/**
+	 * This method allows users to reconnect to a Session that was closed after a
+	 * node crashed
+	 */
+	@RequestMapping(value = "/reconnect", method = RequestMethod.POST)
+	public ResponseEntity<?> reconnectAfterCrash(@RequestBody Map<String, Object> params) {
+
+		String sessionId = (String) params.get("sessionId");
+
+		if (this.crashedSessions.contains(sessionId)) {
+			log.warn("A user wants to reconnect to a crashed Session, but Session {} is still being rebuilt",
+					sessionId);
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		}
+
+		if (this.sessions.containsKey(sessionId)) {
+			log.info("A user is reconnecting to Session {}", sessionId);
+			return returnToken(this.sessions.get(sessionId));
+		}
+
+		log.error("A user is asking to reconnect to Sesion {} but it does not exist", sessionId);
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	/**
+	 * This method returns the Media Nodes information of the cluster
+	 */
+	@RequestMapping(value = "/cluster-status", method = RequestMethod.GET)
+	public ResponseEntity<?> getClusterStatus() throws Exception {
+
+		log.info("Getting cluster status");
+
+		HttpGet request = new HttpGet(this.OPENVIDU_URL + "openvidu/api/media-nodes");
+		HttpResponse response;
+		try {
+			response = HTTP_CLIENT.execute(request);
+		} catch (IOException e) {
+			return getErrorResponse(e);
+		}
+
+		return new ResponseEntity<>(httpResponseToJson(response), HttpStatus.OK);
+	}
+
+	/**
+	 * This is the webhook handler. OpenVidu webhook events are received in this
+	 * method
+	 */
+	@RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<JsonObject> webhook(@RequestBody Map<String, Object> event) throws Exception {
+
+		log.info("Webhook event received: {}", event.get("event"));
+
+		String eventName = (String) event.get("event");
+
+		// Handle the necessary webhook events. Do not block the return of 200 OK to
+		// OpenVidu Server
+		switch (eventName) {
+		case "nodeCrashed":
+			new Thread(() -> {
+				handleNodeCrashedEvent(event);
+			}).start();
+			break;
+		case "sessionDestroyed":
+			new Thread(() -> {
+				handleSessionDestroyedEvent(event);
+			}).start();
+			break;
+		}
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	private void handleNodeCrashedEvent(Map<String, Object> event) {
+
+		log.info("Media Node {} has crashed", event.get("id"));
+		log.info("Affected users will ask to rejoin a new Session");
+
+		List<String> affectedSessions = (List<String>) event.get("sessionIds");
+		affectedSessions.forEach(sessionId -> {
+			// Remove the Session from our collection of active Sessions
+			sessions.remove(sessionId);
+			// Store the Session as crashed and waiting to be rebuilt
+			crashedSessions.add(sessionId);
+		});
+	}
+
+	private void handleSessionDestroyedEvent(Map<String, Object> event) {
+
+		if ("nodeCrashed".equals(event.get("reason"))) {
+
+			String sessionId = (String) event.get("sessionId");
+
+			// The new Session has the same customSessionId as the previous one
+			SessionProperties props = new SessionProperties.Builder().customSessionId(sessionId).build();
+
+			// Create and store the new Session
+			try {
+				Session newSession = openVidu.createSession(props);
+				sessions.put(sessionId, newSession);
+			} catch (OpenViduJavaClientException | OpenViduHttpException e) {
+				log.error("Error creating new Session after nodeCrashed: {}", e.getMessage());
+				return;
+			}
+
+			// Remove the Session from the collection of crashed Sessions
+			this.crashedSessions.remove(sessionId);
+		}
+	}
+
+	private ResponseEntity<?> createSessionAndReturnToken(String sessionId) {
+		try {
+
+			// Create a new OpenVidu Session
+			SessionProperties props = new SessionProperties.Builder().customSessionId(sessionId).build();
+			Session session = this.openVidu.createSession(props);
+
+			// Store the Session in our collection
+			this.sessions.put(sessionId, session);
+
+			// Return a token
+			return this.returnToken(session);
+
+		} catch (Exception e) {
+			// If error generate an error message and return it to the client
+			return getErrorResponse(e);
+		}
+	}
+
+	private ResponseEntity<?> returnToken(Session session) {
+		try {
+
+			String token = session.createConnection().getToken();
+
+			// Send the response with the token
+			JsonObject responseJson = new JsonObject();
+			responseJson.addProperty("token", token);
+			return new ResponseEntity<>(responseJson, HttpStatus.OK);
+
+		} catch (OpenViduJavaClientException e1) {
+			// If internal error generate an error message and return it to client
+			return getErrorResponse(e1);
+		} catch (OpenViduHttpException e2) {
+			if (404 == e2.getStatus()) {
+				// Invalid sessionId (user left unexpectedly). Session object is not valid
+				// anymore. Clean collection and return error. A new Session should be created
+				this.sessions.remove(session.getSessionId());
+			}
+			return getErrorResponse(e2);
+		}
+	}
+
+	private ResponseEntity<JsonObject> getErrorResponse(Exception e) {
+		JsonObject json = new JsonObject();
+		if (e.getCause() != null) {
+			json.addProperty("cause", e.getCause().toString());
+		}
+		if (e.getStackTrace() != null) {
+			json.addProperty("stacktrace", e.getStackTrace().toString());
+		}
+		json.addProperty("error", e.getMessage());
+		json.addProperty("exception", e.getClass().getCanonicalName());
+		return new ResponseEntity<>(json, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	private JsonObject httpResponseToJson(HttpResponse response) throws Exception {
+		try {
+			JsonObject json = new Gson().fromJson(EntityUtils.toString(response.getEntity(), "UTF-8"),
+					JsonObject.class);
+			return json;
+		} catch (JsonSyntaxException | ParseException | IOException e) {
+			throw new Exception(e.getMessage(), e.getCause());
+		}
+	}
+
+	/**
+	 * This
+	 */
+	private void initInsecureHttpClient() {
+		TrustStrategy trustStrategy = new TrustStrategy() {
+			@Override
+			public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				return true;
+			}
+		};
+
+		CredentialsProvider provider = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("OPENVIDUAPP", this.SECRET);
+		provider.setCredentials(AuthScope.ANY, credentials);
+
+		SSLContext sslContext;
+
+		try {
+			sslContext = new SSLContextBuilder().loadTrustMaterial(null, trustStrategy).build();
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+			throw new RuntimeException(e);
+		}
+
+		RequestConfig.Builder requestBuilder = RequestConfig.custom();
+		requestBuilder = requestBuilder.setConnectTimeout(30000);
+		requestBuilder = requestBuilder.setConnectionRequestTimeout(30000);
+
+		HTTP_CLIENT = HttpClientBuilder.create().setDefaultRequestConfig(requestBuilder.build())
+				.setConnectionTimeToLive(30, TimeUnit.SECONDS).setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+				.setSSLContext(sslContext).setDefaultCredentialsProvider(provider).build();
+	}
+
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,308 @@
+var OV;
+var session;
+
+var sessionId;
+var numVideos;
+
+
+/* OPENVIDU METHODS */
+
+function joinSession() {
+	getToken(token => connectToSessionWithToken(token));
+}
+
+function leaveSession() {
+	session.disconnect();
+}
+
+function connectToSessionWithToken(token) {
+
+	numVideos = 0;
+
+	OV = new OpenVidu();
+
+	session = OV.initSession();
+
+	session.on('connectionCreated', event => {
+		pushEvent(event);
+	});
+
+	session.on('connectionDestroyed', event => {
+		pushEvent(event);
+	});
+
+	session.on('streamCreated', event => {
+		pushEvent(event);
+
+		// Subscribe to the Stream to receive it
+		// HTML video will be appended to element with 'video-container' id
+		var subscriber = session.subscribe(event.stream, 'video-container');
+
+		// When the HTML video has been appended to DOM...
+		subscriber.on('videoElementCreated', event => {
+			pushEvent(event);
+			// Add a new HTML element for the user's name and nickname over its video
+			updateNumVideos(1);
+		});
+
+		// When the HTML video has been appended to DOM...
+		subscriber.on('videoElementDestroyed', event => {
+			pushEvent(event);
+			// Add a new HTML element for the user's name and nickname over its video
+			updateNumVideos(-1);
+		});
+
+		// When the subscriber stream has started playing media...
+		subscriber.on('streamPlaying', event => {
+			pushEvent(event);
+		});
+	});
+
+	session.on('streamDestroyed', event => {
+		pushEvent(event);
+	});
+
+	session.on('sessionDisconnected', event => {
+		pushEvent(event);
+		session = null;
+		numVideos = 0;
+		if (event.reason === 'nodeCrashed') {
+			console.warn('Media Node has crashed!');
+			$('#reconnectionModal').modal('show');
+			tryToReconnect(250);
+		} else {
+			$('#join').show();
+			$('#session').hide();
+		}
+	});
+
+	session.connect(token)
+		.then(() => {
+
+			// Set page layout for active call
+
+			$('#session-title').text(sessionId);
+			$('#join').hide();
+			$('#session').show();
+
+			// Get your own camera stream
+
+			var publisher = OV.initPublisher('video-container', {
+				audioSource: undefined, // The source of audio. If undefined default microphone
+				videoSource: undefined, // The source of video. If undefined default webcam
+				publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+				publishVideo: true, // Whether you want to start publishing with your video enabled or not
+				resolution: '640x480', // The resolution of your video
+				frameRate: 30, // The frame rate of your video
+				insertMode: 'APPEND', // How the video is inserted in the target element 'video-container'
+				mirror: false // Whether to mirror your local video or not
+			});
+
+			// Specify the actions when events take place in our publisher
+
+			// When the publisher stream has started playing media...
+			publisher.on('accessAllowed', event => {
+				pushEvent({
+					type: 'accessAllowed'
+				});
+			});
+
+			publisher.on('accessDenied', event => {
+				pushEvent(event);
+			});
+
+			publisher.on('accessDialogOpened', event => {
+				pushEvent({
+					type: 'accessDialogOpened'
+				});
+			});
+
+			publisher.on('accessDialogClosed', event => {
+				pushEvent({
+					type: 'accessDialogClosed'
+				});
+			});
+
+			// When the publisher stream has started playing media...
+			publisher.on('streamCreated', event => {
+				pushEvent(event);
+			});
+
+			// When our HTML video has been added to DOM...
+			publisher.on('videoElementCreated', event => {
+				pushEvent(event);
+				updateNumVideos(1);
+				$(event.element).prop('muted', true); // Mute local video
+			});
+
+			// When the HTML video has been appended to DOM...
+			publisher.on('videoElementDestroyed', event => {
+				pushEvent(event);
+				// Add a new HTML element for the user's name and nickname over its video
+				updateNumVideos(-1);
+			});
+
+			// When the publisher stream has started playing media...
+			publisher.on('streamPlaying', event => {
+				pushEvent(event);
+			});
+
+			// Publish your stream
+
+			session.publish(publisher);
+
+		})
+		.catch(error => {
+			console.warn('There was an error connecting to the session:', error.code, error.message);
+		});
+
+	return false;
+}
+
+/* OPENVIDU METHODS */
+
+
+
+/* APPLICATION REST METHODS */
+
+function getToken(callback) {
+	sessionId = $("#sessionId").val(); // Video-call chosen by the user
+
+	httpRequest(
+		'POST',
+		'api/get-token', {
+			sessionId: sessionId
+		},
+		'Request of TOKEN gone WRONG:',
+		res => {
+			token = res['token']; // Get token from response
+			console.log('Request of TOKEN gone WELL (TOKEN:' + token + ')');
+			callback(token); // Continue the join operation
+		}
+	);
+}
+
+function refreshClusterStatus() {
+	httpRequest(
+		'GET',
+		'api/cluster-status',
+		undefined,
+		'Request of cluster info gone WRONG:',
+		res => {
+			console.log('Request of cluster info gone WELL', res);
+			var stringResponse = 'Number of Media Nodes: ' + res.numberOfElements + '\n';
+			stringResponse += '─────────────────────\n';
+			for (var mediaNode of res.content) {
+				stringResponse += '    • ID:' + mediaNode.id + '\n';
+				stringResponse += '    • Environment ID: ' + mediaNode.environmentId + '\n';
+				stringResponse += '    • IP: ' + mediaNode.ip + '\n';
+				stringResponse += '    • Connected: ' + mediaNode.connected + '\n';
+				stringResponse += '    • CPU load: ' + mediaNode.load + ' %\n';
+				stringResponse += '    • Status: ' + mediaNode.status + '\n';
+				stringResponse += '─────────────────────\n';
+			}
+			$('#textarea-cluster').text(stringResponse);
+		}
+	);
+}
+
+function tryToReconnect(waitTimeoutMs) {
+	httpRequest(
+		'POST',
+		'api/reconnect', {
+			sessionId: sessionId
+		},
+		'Request of reconnection after node crash gone WRONG:',
+		res => {
+			token = res['token']; // Get token from response
+			console.log('Request reconnection after node crash gone WELL (TOKEN:' + token + ')');
+			$('#reconnectionModal').modal('hide');
+			connectToSessionWithToken(token);
+		},
+		errorHttp => {
+			if (errorHttp.status === 409) {
+				// Still waiting fot the backend to rebuild the broken session
+				setTimeout(() => tryToReconnect(waitTimeoutMs), waitTimeoutMs);
+			}
+		}
+	);
+}
+
+function httpRequest(method, url, body, errorMsg, successCallback, errorCallback) {
+	var http = new XMLHttpRequest();
+	http.open(method, url, true);
+	http.addEventListener('readystatechange', processRequest, false);
+	if (!!body) {
+		http.setRequestHeader('Content-type', 'application/json');
+		http.send(JSON.stringify(body));
+	} else {
+		http.send();
+	}
+
+	function processRequest() {
+		if (http.readyState == 4) {
+			if (http.status == 200) {
+				try {
+					successCallback(JSON.parse(http.responseText));
+				} catch (e) {
+					successCallback(e);
+				}
+			} else {
+				console.warn(errorMsg + ' (' + http.status + ')');
+				console.warn(http.responseText);
+				if (errorCallback != null) {
+					errorCallback(http);
+				}
+			}
+		}
+	}
+}
+
+/* APPLICATION REST METHODS */
+
+
+
+/* APPLICATION BROWSER METHODS */
+
+events = '';
+
+window.onbeforeunload = function () { // Gracefully leave session
+	if (session) {
+		leaveSession();
+	}
+}
+
+function updateNumVideos(i) {
+	numVideos += i;
+	$('video').removeClass();
+	switch (numVideos) {
+		case 1:
+			$('video').addClass('two');
+			break;
+		case 2:
+			$('video').addClass('two');
+			break;
+		case 3:
+			$('video').addClass('three');
+			break;
+		case 4:
+			$('video').addClass('four');
+			break;
+	}
+}
+
+function pushEvent(event) {
+	events += (!events ? '' : '\n') + event.type;
+	$('#textarea-events').text(events);
+}
+
+function clearEventsTextarea() {
+	$('#textarea-events').text('');
+	events = '';
+}
+
+function clearClusterTextarea() {
+	$('#textarea-cluster').text('');
+}
+
+/* APPLICATION BROWSER METHODS */
